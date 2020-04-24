e1be4325d7c06661230cb3c8738d53cf6a072e72
Shivam Goswami <shiv.goswami@actewagl.com.au>
2020-04-25 00:06:14 +1000

    Added products section

--- src/components/ContactBtn.vue
+++ src/components/ContactBtn.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-btn color="primary" rounded height="50" width="150" :text="!backgroundColor"
+    <v-btn color="primary" rounded height="50" width="150" :text="!backgroundColor" :outlined="outlined"
            class="font-weight-bold text-capitalize my-5">
         {{text}}
     </v-btn>
@@ -15,6 +15,10 @@
         backgroundColor: {
           type: Boolean,
           default: true
+        },
+        outlined: {
+          type: Boolean,
+          default: false
         }
       }
     }
